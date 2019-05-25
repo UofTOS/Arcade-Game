17d3a61012dfbd3883e0032546b3aefa6122f72c
AdityaGoyal1999 <44474678+AdityaGoyal1999@users.noreply.github.com>
2019-05-26 00:38:03 +0530

    Add files via upload
    Assets to be used in the graphics of the game

---
+++ Console.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Console : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int var;
+    private Material mat;
+    public Color newColor;
+    public KeyCode changecol;
+
+    public void awake()
+    {
+        Debug.Log (" This is when the awake module is being called at the beginning of the code.");
+    }
+
+    void Start()
+    {
+        mat.color = Color.red;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mat.color = newColor;
+    }
+}
